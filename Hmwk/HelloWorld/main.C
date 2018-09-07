943204fd8f6855f946d3f664934a5830e929a04f
Uriel Salazar <usalazar1@student.rccd.edu>
2018-09-06 23:11:52 -0700

    Testing IDE Assignment

---
+++ Hmwk/HelloWorld/main.C
@@ -0,0 +1,25 @@
+/* 
+@file:      main.C
+@author:    Uriel Salazar
+@date:      September 6th, 2018
+@section:   47986
+@brief:     Hello World program to ensure IDE environment is working correctly.
+ */
+
+//System Libraries
+#include <iostream> //Input/Output library
+
+//User Libraries
+
+//Global Constants
+
+//Function Prototypes
+
+//Execution begins here
+int main(int argc, char** argv)
+{
+    std::cout << "Hello, world!" << std::endl;
+
+    //Exit program
+    return 0;
+}
